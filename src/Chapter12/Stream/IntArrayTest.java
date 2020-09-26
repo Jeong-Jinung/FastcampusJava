bda09059bfaf6138071d5287b6e9d12b51bdf14a
DevJeong <jinung.dev@gmail.com>
2020-09-27 00:05:52 +0900

    스트림 정리

---
+++ src/Chapter12/Stream/IntArrayTest.java
@@ -0,0 +1,17 @@
+package Chapter12.Stream;
+
+import java.util.Arrays;
+
+public class IntArrayTest {
+  public static void main(String[] args) {
+
+    int[] arr = {1, 2, 3, 4, 5};
+
+    int sum = Arrays.stream(arr).sum();
+    int count = (int) Arrays.stream(arr).count();
+
+    System.out.println(sum);
+    System.out.println(count);
+
+  }
+}
